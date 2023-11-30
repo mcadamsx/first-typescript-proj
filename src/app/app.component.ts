ec512f79de3a6cad12e4d8a4865f7d3abdb9258b
Richard McAdams <93725119+mcthekreator@users.noreply.github.com>
2023-11-30 18:33:33 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, NavbarComponent, BannerComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+
+})
+export class AppComponent {
+  title = 'proj-5';
+}
